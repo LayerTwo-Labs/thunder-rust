e2626fe8ecc32b2e639cab7e248e236b7ee2d9cd
Ash Manning <ash@layertwolabs.com>
2024-03-06 18:00:38 +0800

    Bump eframe for windows compat. Possibly breaks wayland support. Bump version number

--- shell.nix
+++ shell.nix
@@ -14,14 +14,22 @@ pkgs.mkShell {
   ];
   # Needed for X11
   shellHook =
-    let x11-libs = with pkgs; lib.makeLibraryPath [
+    let
+      common-libs = with pkgs; lib.makeLibraryPath [
         libGL
+      ];
+      wayland-libs = with pkgs; lib.makeLibraryPath [
+        libxkbcommon
+        wayland
+      ];
+      x11-libs = with pkgs; lib.makeLibraryPath [
         xorg.libX11
         xorg.libXcursor
         xorg.libXi
         xorg.libXrandr
-    ]; in ''
-      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${x11-libs}"
+      ];
+    in ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${common-libs}:${wayland-libs}:${x11-libs}"
       export BOOST_LIB_DIR="${pkgs.boost.out}/lib"
     '';
 }
