0bb78e987e12d34ff156098fb18f882be74f1f69
Ash Manning <ash@layertwolabs.com>
2024-05-29 18:28:09 +0900

    Add rolling file logger, bump version number for release

--- app/cli.rs
+++ app/cli.rs
@@ -1,43 +1,145 @@
-use clap::Parser;
-use std::{net::SocketAddr, path::PathBuf};
+use clap::{Arg, Parser};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    ops::Deref,
+    path::PathBuf,
+    sync::LazyLock,
+};
+
+const fn ipv4_socket_addr(ipv4_octets: [u8; 4], port: u16) -> SocketAddr {
+    let [a, b, c, d] = ipv4_octets;
+    let ipv4 = Ipv4Addr::new(a, b, c, d);
+    SocketAddr::new(IpAddr::V4(ipv4), port)
+}
+
+static DEFAULT_DATA_DIR: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| match dirs::data_dir() {
+        None => {
+            tracing::warn!("Failed to resolve default data dir");
+            None
+        }
+        Some(data_dir) => Some(data_dir.join("thunder")),
+    });
+
+const DEFAULT_MAIN_ADDR: SocketAddr = ipv4_socket_addr([127, 0, 0, 1], 18443);
+
+const DEFAULT_MAIN_USER: &str = "user";
+
+const DEFAULT_MAIN_PASS: &str = "password";
+
+const DEFAULT_NET_ADDR: SocketAddr = ipv4_socket_addr([0, 0, 0, 0], 4000);
+
+const DEFAULT_RPC_ADDR: SocketAddr = ipv4_socket_addr([127, 0, 0, 1], 2020);
+
+/// Implement arg manually so that there is only a default if we can resolve
+/// the default data dir
+#[derive(Clone, Debug)]
+#[repr(transparent)]
+struct DatadirArg(PathBuf);
+
+impl clap::FromArgMatches for DatadirArg {
+    fn from_arg_matches(
+        matches: &clap::ArgMatches,
+    ) -> Result<Self, clap::Error> {
+        let mut matches = matches.clone();
+        Self::from_arg_matches_mut(&mut matches)
+    }
+
+    fn from_arg_matches_mut(
+        matches: &mut clap::ArgMatches,
+    ) -> Result<Self, clap::Error> {
+        let datadir = matches
+            .remove_one::<PathBuf>("DATADIR")
+            .expect("`datadir` is required");
+        Ok(Self(datadir))
+    }
+
+    fn update_from_arg_matches(
+        &mut self,
+        matches: &clap::ArgMatches,
+    ) -> Result<(), clap::Error> {
+        let mut matches = matches.clone();
+        self.update_from_arg_matches_mut(&mut matches)
+    }
+
+    fn update_from_arg_matches_mut(
+        &mut self,
+        matches: &mut clap::ArgMatches,
+    ) -> Result<(), clap::Error> {
+        if let Some(datadir) = matches.remove_one("DATADIR") {
+            self.0 = datadir;
+        }
+        Ok(())
+    }
+}
+
+impl clap::Args for DatadirArg {
+    fn augment_args(cmd: clap::Command) -> clap::Command {
+        cmd.arg({
+            let arg = Arg::new("DATADIR")
+                .value_parser(clap::builder::PathBufValueParser::new())
+                .long("datadir")
+                .short('d')
+                .help("Data directory for storing blockchain and wallet data");
+            match DEFAULT_DATA_DIR.deref() {
+                None => arg.required(true),
+                Some(datadir) => {
+                    arg.required(false).default_value(datadir.as_os_str())
+                }
+            }
+        })
+    }
+
+    fn augment_args_for_update(cmd: clap::Command) -> clap::Command {
+        Self::augment_args(cmd)
+    }
+}
 
 #[derive(Clone, Debug, Parser)]
 #[command(author, version, about, long_about = None)]
-pub struct Cli {
-    /// data directory for storing blockchain data and wallet, defaults to ~/.local/share
-    #[arg(short, long)]
-    pub datadir: Option<PathBuf>,
+pub(super) struct Cli {
+    /// Data directory for storing blockchain and wallet data
+    #[command(flatten)]
+    datadir: DatadirArg,
     /// If specified, the gui will not launch.
     #[arg(long)]
-    pub headless: bool,
-    /// Log level, defaults to [`tracing::Level::Debug`]
+    headless: bool,
+    /// Directory in which to store log files.
+    /// Defaults to `<DATADIR>/logs/v<VERSION>`, where `<DATADIR>` is thunder's data
+    /// directory, and `<VERSION>` is the thunder app version.
+    /// By default, only logs at the WARN level and above are logged to file.
+    /// If set to the empty string, logging to file will be disabled.
+    #[arg(long)]
+    log_dir: Option<PathBuf>,
+    /// Log level
     #[arg(default_value_t = tracing::Level::DEBUG, long)]
-    pub log_level: tracing::Level,
-    /// address to use for P2P networking, defaults to 0.0.0.0:4000
-    #[arg(short, long)]
-    pub net_addr: Option<String>,
-    /// address to connect to mainchain node RPC server, defaults to 127.0.0.1:18443
-    #[arg(short, long)]
-    pub main_addr: Option<String>,
+    log_level: tracing::Level,
+    /// Socket address to connect to mainchain node RPC server
+    #[arg(default_value_t = DEFAULT_MAIN_ADDR, long, short)]
+    main_addr: SocketAddr,
     /// Path to a mnemonic seed phrase
     #[arg(long)]
-    pub mnemonic_seed_phrase_path: Option<PathBuf>,
-    /// address for use by the RPC server exposing getblockcount and stop commands, defaults to
-    /// 127.0.0.1:2020
-    #[arg(short, long)]
-    pub rpc_addr: Option<String>,
-    /// mainchain node RPC user, defaults to "user"
-    #[arg(short, long)]
-    pub user_main: Option<String>,
-    /// mainchain node RPC password, defaults to "password"
-    #[arg(short, long)]
-    pub password_main: Option<String>,
+    mnemonic_seed_phrase_path: Option<PathBuf>,
+    /// Socket address to use for P2P networking
+    #[arg(default_value_t = DEFAULT_NET_ADDR, long, short)]
+    net_addr: SocketAddr,
+    /// Socket address to host the RPC server
+    #[arg(default_value_t = DEFAULT_RPC_ADDR, long, short)]
+    rpc_addr: SocketAddr,
+    /// Mainchain node RPC user
+    #[arg(default_value_t = DEFAULT_MAIN_USER.to_owned(), long, short)]
+    user_main: String,
+    /// Mainchain node RPC password
+    #[arg(default_value_t = DEFAULT_MAIN_PASS.to_owned(), long, short)]
+    password_main: String,
 }
 
 #[derive(Clone, Debug)]
 pub struct Config {
     pub datadir: PathBuf,
     pub headless: bool,
+    /// If None, logging to file should be disabled.
+    pub log_dir: Option<PathBuf>,
     pub log_level: tracing::Level,
     pub main_addr: SocketAddr,
     pub main_password: String,
@@ -49,47 +151,33 @@ pub struct Config {
 
 impl Cli {
     pub fn get_config(self) -> anyhow::Result<Config> {
-        let datadir = self
-            .datadir
-            .clone()
-            .unwrap_or_else(|| {
-                dirs::data_dir()
-                    .expect("couldn't get default datadir, specify --datadir")
-            })
-            .join("thunder");
-        const DEFAULT_MAIN_ADDR: &str = "127.0.0.1:18443";
-        let main_addr: SocketAddr = self
-            .main_addr
-            .clone()
-            .unwrap_or(DEFAULT_MAIN_ADDR.to_string())
-            .parse()?;
-        let main_password = self
-            .password_main
-            .clone()
-            .unwrap_or_else(|| "password".into());
-        let main_user = self.user_main.clone().unwrap_or_else(|| "user".into());
-        const DEFAULT_NET_ADDR: &str = "0.0.0.0:4000";
-        let net_addr: SocketAddr = self
-            .net_addr
-            .clone()
-            .unwrap_or(DEFAULT_NET_ADDR.to_string())
-            .parse()?;
-        const DEFAULT_RPC_ADDR: &str = "127.0.0.1:2020";
-        let rpc_addr: SocketAddr = self
-            .rpc_addr
-            .clone()
-            .unwrap_or(DEFAULT_RPC_ADDR.to_string())
-            .parse()?;
+        let log_dir = match self.log_dir {
+            None => {
+                let version_dir_name =
+                    format!("v{}", env!("CARGO_PKG_VERSION"));
+                let log_dir =
+                    self.datadir.0.join("logs").join(version_dir_name);
+                Some(log_dir)
+            }
+            Some(log_dir) => {
+                if log_dir.as_os_str().is_empty() {
+                    None
+                } else {
+                    Some(log_dir)
+                }
+            }
+        };
         Ok(Config {
-            datadir,
+            datadir: self.datadir.0,
             headless: self.headless,
+            log_dir,
             log_level: self.log_level,
-            main_addr,
-            main_password,
-            main_user,
+            main_addr: self.main_addr,
+            main_password: self.password_main,
+            main_user: self.user_main,
             mnemonic_seed_phrase_path: self.mnemonic_seed_phrase_path,
-            net_addr,
-            rpc_addr,
+            net_addr: self.net_addr,
+            rpc_addr: self.rpc_addr,
         })
     }
 }
--- app/main.rs
+++ app/main.rs
@@ -1,10 +1,12 @@
 #![feature(let_chains)]
 
-use std::sync::mpsc;
+use std::{path::Path, sync::mpsc};
 
 use clap::Parser as _;
 
-use tracing_subscriber::{filter as tracing_filter, layer::SubscriberExt};
+use tracing_subscriber::{
+    filter as tracing_filter, layer::SubscriberExt, Layer,
+};
 
 mod app;
 mod cli;
@@ -15,8 +17,45 @@ mod util;
 
 use line_buffer::{LineBuffer, LineBufferWriter};
 
-// Configure logger
-fn set_tracing_subscriber(log_level: tracing::Level) -> LineBuffer {
+/// Must be held for the lifetime of the program in order to keep the file
+/// logger alive.
+type RollingLoggerGuard = tracing_appender::non_blocking::WorkerGuard;
+
+/// Rolling file logger.
+/// Returns a guard that must be held for the lifetime of the program in order
+/// to keep the file logger alive.
+fn rolling_logger<S>(
+    log_dir: &Path,
+) -> anyhow::Result<(impl Layer<S>, RollingLoggerGuard)>
+where
+    S: tracing::Subscriber
+        + for<'s> tracing_subscriber::registry::LookupSpan<'s>,
+{
+    const DEFAULT_LEVEL: tracing::Level = tracing::Level::WARN;
+    const LOG_FILE_SUFFIX: &str = "log";
+    let rolling_log_appender = tracing_appender::rolling::Builder::new()
+        .rotation(tracing_appender::rolling::Rotation::DAILY)
+        .filename_suffix(LOG_FILE_SUFFIX)
+        .build(log_dir)?;
+    let (non_blocking_rolling_log_writer, rolling_log_guard) =
+        tracing_appender::non_blocking(rolling_log_appender);
+    let level_filter =
+        tracing_filter::Targets::new().with_default(DEFAULT_LEVEL);
+    let rolling_log_layer = tracing_subscriber::fmt::layer()
+        .compact()
+        .with_ansi(false)
+        .with_writer(non_blocking_rolling_log_writer)
+        .with_filter(level_filter);
+    Ok((rolling_log_layer, rolling_log_guard))
+}
+
+// Configure loggers.
+// If the file logger is set, returns a guard that must be held for the
+// lifetime of the program in order to keep the file logger alive.
+fn set_tracing_subscriber(
+    log_dir: Option<&Path>,
+    log_level: tracing::Level,
+) -> anyhow::Result<(LineBuffer, Option<RollingLoggerGuard>)> {
     let targets_filter = tracing_filter::Targets::new().with_targets([
         ("bip300301", log_level),
         ("jsonrpsee_core::tracing", log_level),
@@ -27,6 +66,13 @@ fn set_tracing_subscriber(log_level: tracing::Level) -> LineBuffer {
     let stdout_layer = tracing_subscriber::fmt::layer()
         .compact()
         .with_line_number(true);
+    let (rolling_log_layer, rolling_log_guard) = match log_dir {
+        None => (None, None),
+        Some(log_dir) => {
+            let (layer, guard) = rolling_logger(log_dir)?;
+            (Some(layer), Some(guard))
+        }
+    };
     let capture_layer = tracing_subscriber::fmt::layer()
         .compact()
         .with_line_number(true)
@@ -35,16 +81,18 @@ fn set_tracing_subscriber(log_level: tracing::Level) -> LineBuffer {
     let tracing_subscriber = tracing_subscriber::registry()
         .with(targets_filter)
         .with(stdout_layer)
-        .with(capture_layer);
+        .with(capture_layer)
+        .with(rolling_log_layer);
     tracing::subscriber::set_global_default(tracing_subscriber)
         .expect("setting default subscriber failed");
-    line_buffer
+    Ok((line_buffer, rolling_log_guard))
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
     let config = cli.get_config()?;
-    let line_buffer = set_tracing_subscriber(config.log_level);
+    let (line_buffer, _rolling_log_guard) =
+        set_tracing_subscriber(config.log_dir.as_deref(), config.log_level)?;
     let app = app::App::new(&config)?;
     // spawn rpc server
     app.runtime.spawn({
