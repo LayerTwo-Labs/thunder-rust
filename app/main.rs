06094a22edbd68890b2688bd31f455f7fdb9add9
Torkel Rogstad <torkel@rogstad.io>
2025-01-30 09:07:56 +0100

    multi: refactor tokio runtime handling, listen for multiple signals
    The motivation for this is being able to pick up multiple different error sources that need to be propagated out from the main function. Prior to this commit, for example being unable to start the RPC server would not end in a fatal error.

--- app/main.rs
+++ app/main.rs
@@ -1,9 +1,10 @@
 #![feature(let_chains)]
 
-use std::{path::Path, sync::mpsc};
+use std::path::Path;
 
 use clap::Parser as _;
 
+use tokio::{signal::ctrl_c, sync::oneshot};
 use tracing_subscriber::{
     filter as tracing_filter, fmt::format, layer::SubscriberExt, Layer,
 };
@@ -148,55 +149,65 @@ fn main() -> anyhow::Result<()> {
         config.log_level,
         config.log_level_file,
     )?;
-    let app: Result<app::App, app::Error> = app::App::new(&config)
-        .inspect(|app| {
-            // spawn rpc server
-            app.runtime.spawn({
-                let app = app.clone();
-                async move {
-                    rpc_server::run_server(app, config.rpc_addr).await.unwrap()
-                }
-            });
-        })
-        .inspect_err(|err| {
-            tracing::error!("application error: {:?}", err);
-        });
 
-    if config.headless {
-        tracing::info!("Running in headless mode");
-        drop(line_buffer);
-        let _app = app?;
-        // wait for ctrlc signal
-        let (tx, rx) = mpsc::channel();
-        ctrlc::set_handler(move || {
-            tx.send(()).unwrap();
-        })
-        .expect("Error setting Ctrl-C handler");
-        rx.recv().unwrap();
-        tracing::info!("Received Ctrl-C signal, exiting...");
-    } else {
-        let native_options = eframe::NativeOptions::default();
-        let app: Option<_> = app.map_or_else(
-            |err| {
-                let err = anyhow::Error::from(err);
-                tracing::error!("{err:#}");
-                None
-            },
-            Some,
-        );
-        eframe::run_native(
-            "Thunder",
-            native_options,
-            Box::new(move |cc| {
-                Ok(Box::new(gui::EguiApp::new(
-                    app,
-                    cc,
-                    line_buffer,
-                    config.rpc_addr,
-                )))
-            }),
-        )
-        .map_err(|err| anyhow::anyhow!("failed to launch egui app: {err}"))?
+    let (app_tx, app_rx) = oneshot::channel::<anyhow::Error>();
+
+    let app = app::App::new(&config).inspect(|app| {
+        // spawn rpc server
+        app.runtime.spawn({
+            let app = app.clone();
+            async move {
+                tracing::info!("starting RPC server at `{}`", config.rpc_addr);
+                if let Err(err) =
+                    rpc_server::run_server(app, config.rpc_addr).await
+                {
+                    app_tx.send(err).expect("failed to send error to app");
+                }
+            }
+        });
+    });
+
+    if !config.headless {
+        // For GUI mode we want the GUI to start, even if the app fails to start.
+        return run_egui_app(&config, line_buffer, app)
+            .map_err(|e| anyhow::anyhow!("failed to run egui app: {e:#}"));
     }
-    Ok(())
+
+    tracing::info!("Running in headless mode");
+    drop(line_buffer);
+
+    // If we're headless, we want to exit hard if the app fails to start.
+    let app = app?;
+
+    app.runtime.block_on(async move {
+        tokio::select! {
+            Ok(_) = ctrl_c() => {
+                tracing::info!("Shutting down due to process interruption");
+                Ok(())
+            }
+            Ok(err) = app_rx => {
+                Err(anyhow::anyhow!("received error from RPC server: {err:#} ({err:?})"))
+            }
+        }
+    })
+}
+
+fn run_egui_app(
+    config: &crate::cli::Config,
+    line_buffer: LineBuffer,
+    app: Result<crate::app::App, crate::app::Error>,
+) -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Thunder",
+        native_options,
+        Box::new(move |cc| {
+            Ok(Box::new(gui::EguiApp::new(
+                app.ok(),
+                cc,
+                line_buffer,
+                config.rpc_addr,
+            )))
+        }),
+    )
 }
