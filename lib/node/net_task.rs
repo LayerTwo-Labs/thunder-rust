a25ef3f9007508e4652fba93dbafb60332a5ded0
Ash Manning <ash@layertwolabs.com>
2026-05-14 18:49:34 +0800

    Update to latest bip300301_enforcer

--- lib/node/net_task.rs
+++ lib/node/net_task.rs
@@ -8,6 +8,7 @@ use std::{
     time::Duration,
 };
 
+use error_fatality::{Nested as _, Split};
 use fallible_iterator::{FallibleIterator, IteratorExt};
 use futures::{
     StreamExt,
@@ -833,7 +834,7 @@ impl NetTask {
             AcceptConnection(
                 Result<
                     Option<SocketAddr>,
-                    <net::error::AcceptConnection as fatality::Split>::Fatal,
+                    <net::error::AcceptConnection as Split>::Fatal,
                 >,
             ),
             // Forward a mainchain task request, along with the peer that
@@ -857,7 +858,6 @@ impl NetTask {
             let env = self.ctxt.env.clone();
             let net = self.ctxt.net.clone();
             let fut = async move {
-                use fatality::Nested as _;
                 let maybe_socket_addr =
                     net.accept_incoming(env).await.into_nested()?;
 
@@ -874,7 +874,7 @@ impl NetTask {
             Ok(Err(non_fatal_err)) => {
                 // type the error explicitly
                 let non_fatal_err:
-                    <net::error::AcceptConnection as fatality::Split>::Jfyi =
+                    <net::error::AcceptConnection as Split>::Jfyi =
                     non_fatal_err;
                 let non_fatal_err = anyhow::Error::from(non_fatal_err);
                 tracing::error!(
@@ -941,7 +941,7 @@ impl NetTask {
                     }
                     Err(fatal_err) => {
                         // explicitly type error
-                        let fatal_err: <net::error::AcceptConnection as fatality::Split>::Fatal =
+                        let fatal_err: <net::error::AcceptConnection as Split>::Fatal =
                             fatal_err;
                         let fatal_err = anyhow::Error::from(fatal_err);
                         tracing::error!(
