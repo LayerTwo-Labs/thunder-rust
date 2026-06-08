72da7027416e61984c8552f9a153cc50da161a22
/dev/fd0 <alicexbt@protonmail.com>
2026-06-08 09:48:48 +0530

    fix: account for mainchain fee in withdrawal output value

--- lib/wallet.rs
+++ lib/wallet.rs
@@ -249,7 +249,7 @@ impl Wallet {
                 .checked_add(main_fee)
                 .ok_or(AmountOverflowError)?,
         )?;
-        let change = total - value - fee;
+        let change = total - value - fee - main_fee;
 
         let inputs: Vec<_> = coins
             .into_iter()
